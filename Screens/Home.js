2d19a17396ff60a863eac659e8e0d0efa25015a8
Oscar Daniel Amaya Santana <oscar.amaya@eia.edu.co>
2024-03-07 18:59:48 -0500

    Router to home

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,18 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import Home from './Screens/Home';
 
-const Stack = createNativeStackNavigator();
-
-export default function App() {
+export default function Home() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>HOME</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
